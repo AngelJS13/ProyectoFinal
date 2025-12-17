0c812eb825f1fc626d623dd54ede6b37f4e4a743
RubiTS02 <mariana02@micorreo.upp.edu.mx>
2025-12-17 05:24:36 -0600

    Agregado host permitido para Render en vite.config.js

--- tiedita-Frontend-Cliente/vite.config.ts
+++ tiedita-Frontend-Cliente/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['proyectofinal-pmv7.onrender.com']
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
